5fdf8011c018da43e3ec160b0193d0b5005b6081
Christopher L <56335217+chrisalee@users.noreply.github.com>
2021-06-24 23:59:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,29 +7,25 @@
  *  2. INTEGER_ARRAY height
  */
 
-// below does not work for all cases
-// const hurdleRace = (k, height) => {
-    
-//     let minHeight = Math.min.apply(Math, height);
-//     console.log("minHeight ", minHeight);
-//     const heightFilter = height.filter((item) => item > (k + minHeight));
-//     console.log('heightFilter ', heightFilter);
-//     if(heightFilter.length === 0) {
-//         return 0;
-//     }
-    
-//     const potionsNeededJump = heightFilter.map(item => item - k);
-//     console.log('potionsNeededJump ', potionsNeededJump);
-//     let potionsConsumed = potionsNeededJump.reduce((sum, total) => sum + total, 0);
-    
-//     console.log('potionsConsumed', potionsConsumed);
-//     return potionsConsumed;
-// }
-
 const hurdleRace = (k, height) => {
     
-  const numOfDrinks = height.reduce((acc, cur) => Math.max(acc, cur), height[0]) - k;
-  return Math.max(0, numOfDrinks);
-  
+    let maxHeight = Math.max.apply(Math, height);
+    console.log("maxHeight ", maxHeight);
+    if(maxHeight < k) {
+        return 0;
+    }
+    
+    const potionsNeededJump = maxHeight - k;
+    return potionsNeededJump;
+    
 }
+
+
+// another solution //////////////////////////////////////////////////////////////////
+// const hurdleRace = (k, height) => {
+    
+//   const numOfDrinks = height.reduce((acc, cur) => Math.max(acc, cur), height[0]) - k;
+//   return Math.max(0, numOfDrinks);
+  
+// }
 
